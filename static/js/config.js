cd4dee43c0930bc06fcc93652753479bb4a033dd
Andy <el.andy.barr@gmail.com>
2015-04-23 21:12:25 -0700

    minor config

--- static/js/config.js
+++ static/js/config.js
@@ -2,5 +2,5 @@ angular.module('conference.config', [])
 
     .constant('FB_APP_ID', '932536800093017') // Use your own FB app id
 
-    .constant('SERVER_PATH','http://localhost:8080');
+    .constant('SERVER_PATH','http://52.5.216.65:8080');
 //    .constant('SERVER_PATH','http://ioconf.herokuapp.com');
